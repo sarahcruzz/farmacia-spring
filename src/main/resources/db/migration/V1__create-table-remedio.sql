4630c280da6be04fcdc8b633a62442407dec58a4
sarahcruzz <sarah.cruz0492@gmail.com>
2024-08-01 16:48:27 -0300

    criando as tableas com flyway

---
+++ src/main/resources/db/migration/V1__create-table-remedio.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Remedio (
+    id bigint not null auto_increment,
+    nome varchar(100) not null,
+    via varchar(100) not null,
+    lote varchar(100) not null,
+    quantidade varchar(100) not null,
+    validade varchar(100) not null,
+    laboratorio varchar(100) not null,
+    primary key(id)
+);
